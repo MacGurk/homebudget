f191991f34bd7a0b12572d1d53b874fe2722256c
Oliver Morf <morfoli1@students.zhaw.ch>
2022-07-08 13:00:27 +0200

    simplify transactions view

--- frontend/src/components/transactions/cards/TransactionCards.tsx
+++ frontend/src/components/transactions/cards/TransactionCards.tsx
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Transaction } from '../../../entities/Transaction.entity';
-import TransactionCard from './TransactionCard';
 
 const TransactionCards: React.FC<{ transactions: Transaction[] }> = ({ transactions }) => (
   <>
     {transactions.map((transaction) => (
-      <TransactionCard transaction={transaction} />
+      <div className="card m-3" key={transaction.id}>
+        <ul className="list-group list-group-flush">
+          <li className="list-group-item">Date: {new Date(transaction.date).toLocaleDateString()}</li>
+          <li className="list-group-item">User: {transaction.user.username}</li>
+          <li className="list-group-item">Description: {transaction.description}</li>
+          <li className="list-group-item">Price: {transaction.price}</li>
+          <li className="list-group-item">
+            Actions:
+            <button type="button" className="btn btn-primary btn-sm">
+              <i className="fa fa-edit" />
+            </button>
+            <button type="button" className="btn btn-danger btn-sm">
+              <i className="fa fa-trash" />
+            </button>
+          </li>
+        </ul>
+      </div>
     ))}
   </>
 );
--- frontend/src/components/transactions/table/TransactionsTable.tsx
+++ frontend/src/components/transactions/table/TransactionsTable.tsx
@@ -1,15 +1,12 @@
 import React from 'react';
 import { Transaction } from '../../../entities/Transaction.entity';
-import TransactionRow from './TransactionRow';
 
 const TransactionsTable: React.FC<{ transactions: Transaction[] }> = ({ transactions }) => (
   <div className="mx-3 border rounded">
     <table className="table table-striped" id="transactionTable">
       <thead>
         <tr>
-          <th scope="col" data-card-title>
-            Date
-          </th>
+          <th scope="col">Date</th>
           <th scope="col">User</th>
           <th scope="col">Description</th>
           <th scope="col">Price</th>
@@ -18,7 +15,20 @@ const TransactionsTable: React.FC<{ transactions: Transaction[] }> = ({ transact
       </thead>
       <tbody>
         {transactions.map((transaction) => (
-          <TransactionRow transaction={transaction} />
+          <tr key={transaction.id}>
+            <td data-label="Data">{new Date(transaction.date).toLocaleDateString()}</td>
+            <td data-label="User">{transaction.user.username}</td>
+            <td data-label="Description">{transaction.description}</td>
+            <td data-label="Price">{transaction.price}</td>
+            <td data-label="Actions">
+              <button type="button" className="btn btn-primary btn-sm">
+                <i className="fa fa-edit" />
+              </button>
+              <button type="button" className="btn btn-danger btn-sm">
+                <i className="fa fa-trash" />
+              </button>
+            </td>
+          </tr>
         ))}
       </tbody>
     </table>
