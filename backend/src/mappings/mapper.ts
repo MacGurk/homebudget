9ca6e93113f72c86a0d59e4a450969077475dfab
Oliver Morf <morfoli1@students.zhaw.ch>
2022-07-11 23:12:27 +0200

    cleanup import & reformatting

--- backend/src/mappings/mapper.ts
+++ backend/src/mappings/mapper.ts
@@ -1,13 +1,13 @@
-import {createMap, createMapper} from '@automapper/core';
+import { createMap, createMapper, mapFrom } from '@automapper/core';
 import { sequelize } from '@automapper/sequelize';
-import {UserDto} from '../dto/User.dto';
+import { UserDto } from '../dto/User.dto';
 import User from '../db/models/User';
 import Transaction from '../db/models/Transaction';
-import {TransactionDto} from '../dto/Transaction.dto';
+import { TransactionDto } from '../dto/Transaction.dto';
 
-export const mapper = createMapper(({
+export const mapper = createMapper({
   strategyInitializer: sequelize(),
-}));
+});
 
 export const createMapping = () => {
   createMap(mapper, Transaction, TransactionDto);
