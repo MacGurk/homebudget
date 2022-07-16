c3ed7ad8c6339c32244d6d28a73704de35f873ac
Oliver Morf <morfoli1@students.zhaw.ch>
2022-07-16 13:44:10 +0200

    fixed reactive navigation and active link highlighting

--- frontend/src/components/common/Nav.tsx
+++ frontend/src/components/common/Nav.tsx
@@ -1,36 +1,49 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 import logo from '../../img/logo.png';
 
 function Nav() {
+  const location = useLocation();
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <Link className="navbar-brand" to="/">
-        <img src={logo} height="30" loading="lazy" alt="Logo" />
-      </Link>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavAltMarkup"
-        aria-controls="navbarNavAltMarkup"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon" />
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
-        <ul className="navbar-nav">
-          <li className="nav-item nav-link active">
-            <Link to="/">Dashboard</Link>
-          </li>
-          <li className="nav-item nav-link">
-            <Link to="/transaction">Transactions</Link>
-          </li>
-          <li className="nav-item nav-link">
-            <Link to="/user">Users</Link>
-          </li>
-        </ul>
+      <div className="container-fluid">
+        <Link className="navbar-brand" to="/">
+          <img src={logo} height="30" loading="lazy" alt="Logo" />
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarCollapsable"
+          aria-controls="navbarCollapsable"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon" />
+        </button>
+        <div className="collapse navbar-collapse" id="navbarCollapsable">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <Link className={`nav-link ${location.pathname === '/' ? 'active' : ''}`} to="/">
+                Dashboard
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link
+                className={`nav-link ${location.pathname.includes('/transaction') ? 'active' : ''}`}
+                to="/transaction"
+              >
+                Transactions
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link className={`nav-link ${location.pathname.includes('/user') ? 'active' : ''}`} to="/user">
+                Users
+              </Link>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './components/App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/js/bootstrap.bundle.min';
+import 'bootstrap/dist/js/bootstrap.min';
 import 'font-awesome/css/font-awesome.min.css';
 import 'popper.js/dist/popper.min';
 import { initialiseMapper } from './mapping/mapper';
